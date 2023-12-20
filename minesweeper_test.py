66cf505760186e0d3a12e5e4a523ab47f333611a
IZang123 <146063026+IZang123@users.noreply.github.com>
2023-12-20 17:07:09 +0300

    Update minesweeper_test.py

--- minesweeper_test.py
+++ minesweeper_test.py
@@ -4,23 +4,19 @@ from minesweeper import Minesweeper
 def test_create_minefield():
     minesweeper = Minesweeper(5, 3)
     minesweeper.create_minefield()
-    # Add assertions based on the expected output of create_minefield
 
 def test_set_bombs():
     minesweeper = Minesweeper(5, 3)
     minesweeper.set_bombs()
-    # Add assertions based on the expected state of the minesweeper instance
 
 def test_set_values():
     minesweeper = Minesweeper(5, 3)
     minesweeper.set_values()
-    # Add assertions based on the expected state of the minesweeper instance
 
 def test_nbors():
     minesweeper = Minesweeper(5, 3)
     minesweeper.vis = []  # Reset vis
     minesweeper.nbors(1, 1)
-    # Add assertions based on the expected state of the minesweeper instance after calling nbors
 
 def test_check_over():
     minesweeper = Minesweeper(5, 3)
@@ -30,13 +26,7 @@ def test_check_over():
                                    [' ', ' ', ' ', ' ', ' '],
                                    [' ', ' ', ' ', ' ', ' ']]
     assert minesweeper.check_over() == False
-    # Add more assertions based on different scenarios
 
 def test_show_bombs():
     minesweeper = Minesweeper(5, 3)
     minesweeper.show_bombs()
-    # Add assertions based on the expected state of the minesweeper instance
-
-def test_play_game():
-    # This test is more complex and may involve mocking user input to simulate a game
-    pass
