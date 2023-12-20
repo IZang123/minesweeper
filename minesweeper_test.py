4fb440e4cfe9b604dd694a3f8ceddce05ddd6b51
IZang123 <146063026+IZang123@users.noreply.github.com>
2023-12-20 17:10:25 +0300

    Update minesweeper_test.py

--- minesweeper_test.py
+++ minesweeper_test.py
@@ -2,9 +2,19 @@ import pytest
 from minesweeper import Minesweeper
 
 def test_create_minefield():
+    # Создаем экземпляр Minesweeper с полем 5x3
     minesweeper = Minesweeper(5, 3)
+    
+    # Вызываем метод create_minefield для генерации поля с минами
     minesweeper.create_minefield()
-
+    
+    # Проверяем, что метод create_minefield выводит что-то в консоль
+    # В данном случае, мы просто проверяем, что не выбрасывается исключение при его вызове
+    try:
+        minesweeper.create_minefield()
+    except Exception as e:
+        assert False, f"An unexpected exception occurred: {e}"
+        
 def test_set_bombs():
     minesweeper = Minesweeper(5, 3)
     minesweeper.set_bombs()
